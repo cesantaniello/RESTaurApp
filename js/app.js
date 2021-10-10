fd4a0d0be30735aedc8d0b2dc44675b3df13158a
Administrator <admin@example.com>
2021-10-10 18:17:06 +0200

    7. Muestra los platillos

--- js/app.js
+++ js/app.js
@@ -56,6 +56,22 @@ function obtenerPlatillos() {
 
     fetch(url)
         .then(respuesta => respuesta.json())
-        .then(resultado => console.log(resultado))
+        .then(resultado => mostrarPlatillos(resultado))
         .catch(error => console.log(error));
+}
+
+function mostrarPlatillos(platillos) {
+    const contenido = document.querySelector('#platillos .contenido');
+
+    platillos.forEach(platillo => {
+        const row = document.createElement('DIV');
+        row.classList.add('row');
+
+        const nombre = document.createElement('DIV');
+        nombre.classList.add('col-md-4');
+        nombre.textContent = platillo.nombre;
+
+        row.appendChild(nombre);
+        contenido.appendChild(row);
+    })
 }
