507df13ae2e6a4977838f7ca2bf2e15f3568aefa
Administrator <admin@example.com>
2021-10-10 19:07:09 +0200

    9. Agrega input para cantidades

--- js/app.js
+++ js/app.js
@@ -85,9 +85,21 @@ function mostrarPlatillos(platillos) {
         categoria.classList.add('col-md-3');
         categoria.textContent = categorias[platillo.categoria];
 
+        const inputCantidad = document.createElement('INPUT');
+        inputCantidad.type = 'number';
+        inputCantidad.min = 0;
+        inputCantidad.value = 0;
+        inputCantidad.id = `producto-${platillo.id}`;
+        inputCantidad.classList.add('form-control');
+
+        const agregar = document.createElement('DIV');
+        agregar.classList.add('col-md-2');
+        agregar.appendChild(inputCantidad);
+
         row.appendChild(nombre);
         row.appendChild(precio);
         row.appendChild(categoria);
+        row.appendChild(agregar);
 
         contenido.appendChild(row);
     })
