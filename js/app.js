aab0aa812c62ee69b344e775e5c65bed53cedff4
Administrator <admin@example.com>
2021-10-10 16:49:02 +0200

    6. Consulta API

--- js/app.js
+++ js/app.js
@@ -41,9 +41,21 @@ function guardarCliente() {
 
     //Muestra las secciones
     mostrarSecciones();
+
+    //Obtiene platillos de la API de JSON-Server
+    obtenerPlatillos();
 }
 
 function mostrarSecciones() {
     const seccionesOcultas = document.querySelectorAll('.d-none');
     seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+}
+
+function obtenerPlatillos() {
+    const url = 'http://localhost:4000/platillos';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(resultado => console.log(resultado))
+        .catch(error => console.log(error));
 }
