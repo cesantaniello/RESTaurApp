a934330c482a123c55c6f94b6b3b48ffa53157b3
Administrator <admin@example.com>
2021-10-10 16:39:47 +0200

    5. Muestra contenido al pasar validacion

--- js/app.js
+++ js/app.js
@@ -38,4 +38,12 @@ function guardarCliente() {
     const modalFormulario = document.querySelector('#formulario');
     const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
     modalBootstrap.hide();
+
+    //Muestra las secciones
+    mostrarSecciones();
+}
+
+function mostrarSecciones() {
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
 }
