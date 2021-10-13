f8fdb9fc1f0dab37a626b94d398377474315556a
Administrator <admin@example.com>
2021-10-13 13:35:56 +0200

    24. Muestra totales

--- js/app.js
+++ js/app.js
@@ -392,5 +392,56 @@ function calcularPropina() {
     //Calcula el total a pagar
     const total = subtotal + propina;
 
-    console.log(total);
+    mostrarTotalHTML(subtotal, total, propina);
+}
+
+function mostrarTotalHTML(subtotal, total, propina) {
+    const divTotales = document.createElement('DIV');
+    divTotales.classList.add('total-pagar', 'my-5');
+
+    //Subtotal
+    const subtotalParrafo = document.createElement('P');
+    subtotalParrafo.classList.add('fs-4', 'fw-bold', 'mt-2');
+    subtotalParrafo.textContent = "Subtotal consumo: ";
+
+    const subtotalSpan = document.createElement('SPAN');
+    subtotalSpan.classList.add('fw-normal');
+    subtotalSpan.textContent = `${subtotal} €`;   
+    
+    subtotalParrafo.appendChild(subtotalSpan);
+
+    //Propina
+    const propinaParrafo = document.createElement('P');
+    propinaParrafo.classList.add('fs-4', 'fw-bold', 'mt-2');
+    propinaParrafo.textContent = "Propina: ";
+
+    const propinaSpan = document.createElement('SPAN');
+    propinaSpan.classList.add('fw-normal');
+    propinaSpan.textContent = `${propina} €`;   
+    
+    propinaParrafo.appendChild(propinaSpan);
+
+    //Total
+    const totalParrafo = document.createElement('P');
+    totalParrafo.classList.add('fs-4', 'fw-bold', 'mt-2');
+    totalParrafo.textContent = "Total: ";
+
+    const totalSpan = document.createElement('SPAN');
+    totalSpan.classList.add('fw-normal');
+    totalSpan.textContent = `${total} €`;   
+    
+    totalParrafo.appendChild(totalSpan);    
+
+    //Eliminar el último resultado
+    const totalpagarDiv = document.querySelector('.total-pagar');
+    if(totalpagarDiv){
+        totalpagarDiv.remove();
+    }
+
+    divTotales.appendChild(subtotalParrafo);
+    divTotales.appendChild(propinaParrafo);
+    divTotales.appendChild(totalParrafo);
+    
+    const formulario = document.querySelector('.formulario > div');
+    formulario.appendChild(divTotales);
 }
